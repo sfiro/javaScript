c7f349ff665813293c292e52fbad93445041c84f
Debbie Johan Arredondo <chevi6@gmail.com>
2023-12-05 11:06:24 -0500

    Create main.js
    grafica una función seno y muestra por la consola los valores asociados a la amplitud por cada iteración

---
+++ main.js
@@ -0,0 +1,17 @@
+// grafica una funcion seno y muestra por la consola los valores asociados a la amplitud por cada
+// iteración
+
+window.onload=function(){
+var canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d"),
+          width = canvas.width = window.innerWidth,
+            height = canvas.height = window.innerHeight;
+ 
+  context.translate(0,height/2);
+  context.scale(1,-1);
+  for(var angle=0; angle< Math.PI*2; angle += 0.01){
+    var x = angle*100, y =Math.sin(angle)*100;
+    console.log(Math.sin(angle)*50);
+    context.fillRect(x,y,5,5);
+  }
+}
