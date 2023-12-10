bcb9ce4424f7a608f00f008a47c8a12f20f4dd76
Debbie Johan Arredondo <chevi6@gmail.com>
2023-12-10 17:43:29 -0500

    Create mini1Normalization.js
     Se crea una función normalización la cual encasilla a los valores entre unos valores maximos y minimos.

---
+++ mini1Normalization.js
@@ -0,0 +1,134 @@
+//  Se crea una función normalización la cual encasilla a los valores entre unos valores maximos y minimos. 
+
+window.onload=function(){
+var canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d"),
+  width = canvas.width = window.innerWidth,
+  height = canvas.height = window.innerHeight;
+
+   var vector = {
+   		_x: 1,
+      _y: 0,
+      
+      create: function(x,y){
+      	var obj = Object.create(this);
+        obj.setX(x);
+        obj.setY(y);
+        return obj;
+      },
+      
+      setX: function(value){
+      	this._x = value;
+      },
+      getX: function(){
+      	return this._x;
+      },
+      setY: function(value){
+      	this._y = value;
+      },
+      getY: function(){
+      	return this._y;
+      },
+      setAngle: function(angle){
+      	var length = this.getLength();
+        this._x = Math.cos(angle)*length;
+        this._y = Math.sin(angle)*length;
+      },
+      getAngle: function(){
+      	return Math.atan2(this._y, this._x);
+      },
+      setLength: function(length){
+      	var angle = this.getAngle();
+        this._x = Math.cos(angle)*length;
+        this._y = Math.sin(angle)*length;
+      },
+      getLength: function(){
+      	return Math.sqrt(this._x*this._x + this._y*this._y);
+      },
+      add: function(v2){
+      	return vector.create(this._x+v2.getX(),this._y+v2.getY())
+      },
+      subtract: function(v2){
+      	return vector.create(this._x-v2.getX(),this._y-v2.getY())
+      },
+      multiply: function(v2){
+      	return vector.create(this._x*v2.getX(),this._y*v2.getY())
+      },
+      divide: function(v2){
+      	return vector.create(this._x/v2.getX(),this._y/v2.getY())
+      },
+      addTo: function(v2){
+      	this._x += v2.getX();
+        this._y += v2.getY();
+      },
+      subtractFrom: function(v2){
+      	this._x -= v2.getX();
+        this._y -= v2.getY();
+      },
+      multiplyBy: function(val){
+      	this._x *= val;
+        this._y *= val;
+      },
+      divideBy: function(val){
+      	this._x /= val;
+        this._y /= val;
+      },
+     
+   }
+   
+   var particle = {
+   		position: null,
+      velocity: null,
+      gravity: null,
+      mass: 1,
+      radius: 0,
+      bounce: -1,
+      friction: 1,
+      
+      create: function(x,y,speed,direction,grav){
+      		var obj = Object.create(this);
+          obj.position = vector.create(x,y);
+          obj.velocity = vector.create(0,0);
+          obj.velocity.setLength(speed);
+          obj.velocity.setAngle(direction);
+          obj.gravity = vector.create(0,grav || 0);
+          return obj;
+      },
+      
+      accelerate: function(accel){
+      	this.velocity.addTo(accel);
+      },
+      
+      update: function(){
+      	 this.velocity.multiplyBy(this.friction)
+         this.velocity.addTo(this.gravity)
+      	 this.position.addTo(this.velocity)
+      }
+   
+   };
+   
+   var values = [7, 5, 21, 18, 33, 12, 27, 18, 9, 23, 14, 6, 31, 25, 17, 13, 29],
+       min = Math.min.apply(null, values),
+       max = Math.max.apply(null, values);
+ 
+   function norm(value,min,max){
+      return (value-min)/(max-min);
+   }
+ 
+ 	context.beginPath();
+   for(var i=0;i< values.length;i+=1){
+		var normValue = norm(values[i],max,min),
+    		x = width/(values.length - 1)*i,
+        y = height - height * normValue;
+        
+    if(i==0){
+    	context.moveTo(x,y);
+    }
+    else{
+    	context.lineTo(x,y);
+    }
+   }
+   
+   context.stroke();
+
+ }
