fa589f6ef5633ed47bb543fd18804d99c895170f
Debbie Johan Arredondo <chevi6@gmail.com>
2023-12-11 19:06:21 -0500

    Create 14collisionInRect.js
    se crea las funciones de colisión para el rectangulo y un punto determinado

---
+++ 14collisionInRect.js
@@ -0,0 +1,187 @@
+//  Se crea un objeto que será manejado con los comando del mouse al presionar, 
+// se usan las interrupciones del mouse, al presionar y al dejar de presionar 
+// así se calcula un angulo en el cual será dirigido un cañon 
+// se crea la bala la cual sale disparada al momento de presionar la barra espaciadora 
+
+window.onload=function(){
+var canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d"),
+  width = canvas.width = window.innerWidth,
+  height = canvas.height = window.innerHeight;
+
+   var vector = {
+   		_x: 1,
+      _y: 0,
+      
+      create: function(x,y){
+      	var obj = Object.create(this);
+        obj.setX(x);
+        obj.setY(y);
+        return obj;
+      },
+      
+      setX: function(value){
+      	this._x = value;
+      },
+      getX: function(){
+      	return this._x;
+      },
+      setY: function(value){
+      	this._y = value;
+      },
+      getY: function(){
+      	return this._y;
+      },
+      setAngle: function(angle){
+      	var length = this.getLength();
+        this._x = Math.cos(angle)*length;
+        this._y = Math.sin(angle)*length;
+      },
+      getAngle: function(){
+      	return Math.atan2(this._y, this._x);
+      },
+      setLength: function(length){
+      	var angle = this.getAngle();
+        this._x = Math.cos(angle)*length;
+        this._y = Math.sin(angle)*length;
+      },
+      getLength: function(){
+      	return Math.sqrt(this._x*this._x + this._y*this._y);
+      },
+      add: function(v2){
+      	return vector.create(this._x+v2.getX(),this._y+v2.getY())
+      },
+      subtract: function(v2){
+      	return vector.create(this._x-v2.getX(),this._y-v2.getY())
+      },
+      multiply: function(v2){
+      	return vector.create(this._x*v2.getX(),this._y*v2.getY())
+      },
+      divide: function(v2){
+      	return vector.create(this._x/v2.getX(),this._y/v2.getY())
+      },
+      addTo: function(v2){
+      	this._x += v2.getX();
+        this._y += v2.getY();
+      },
+      subtractFrom: function(v2){
+      	this._x -= v2.getX();
+        this._y -= v2.getY();
+      },
+      multiplyBy: function(val){
+      	this._x *= val;
+        this._y *= val;
+      },
+      divideBy: function(val){
+      	this._x /= val;
+        this._y /= val;
+      },
+     
+   }
+   
+   var particle = {
+   		position: null,
+      velocity: null,
+      gravity: null,
+      mass: 1,
+      radius: 0,
+      bounce: -1,
+      friction: 1,
+      
+      create: function(x,y,speed,direction,grav){
+      		var obj = Object.create(this);
+          obj.position = vector.create(x,y);
+          obj.velocity = vector.create(0,0);
+          obj.velocity.setLength(speed);
+          obj.velocity.setAngle(direction);
+          obj.gravity = vector.create(0,grav || 0);
+          return obj;
+      },
+      
+      accelerate: function(accel){
+      	this.velocity.addTo(accel);
+      },
+      
+      update: function(){
+      	 this.velocity.multiplyBy(this.friction)
+         this.velocity.addTo(this.gravity)
+      	 this.position.addTo(this.velocity)
+      }
+   
+   };
+   
+    function lerp(norm,min,max){
+      return (max-min)*norm + min;
+
+    }
+ 
+   function norm(value,min,max){
+      return (value-min)/(max-min);
+   }
+   
+   function map(value,sourceMin,sourceMax,destMin,destMax){
+   		var n = norm(value,sourceMin,sourceMax);
+      return lerp(n,destMin,destMax)
+   }
+   
+   function clamp(value,min,max){
+      if(value > max) return max;
+      if(value < min) return min;
+      return value;
+      //return Math.min(Math.max(value,min),max);
+   }
+   
+   function distance(p0,p1){
+   		var dx = p1.x - p0.x,
+      		dy = p1.y - p0.y;
+      return Math.sqrt(dx*dx+dy*dy)
+   }
+   
+   function distanceXY(x0,y0,x1,y1){
+   		var dx = x1-x0,
+      		dy = y1-y0;
+      return Math.sqrt(dx*dx+dy*dy);
+   }
+   
+   
+   function circleCollision(c0,c1){
+   		return distance(c0,c1) <= c0.radius +  c1.radius;
+   }
+   
+   function circlePointCollision(x,y,circle){
+   		return distanceXY(x,y, circle.x,circle.y) < circle.radius; 
+   }
+   
+   function pointInRect(x,y,rect){
+   		return inRange(x,rect.x,rect.x+rect.width) && 
+      			 inRange(y, rect.y, rect.y+rect.height);
+   }
+   function inRange(value,min,max){
+   		return value >= min && value <= max;
+   }
+   
+   var circle = {
+   		x: Math.random()*width,
+      y: Math.random()*height,
+      radius: 50+Math.random()*100
+   },
+   		rect = {
+      	x:300*Math.random(),
+        y:200*Math.random(),
+        width: 200,
+        height: 100
+      };
+  
+   
+   document.body.addEventListener("mousemove",function(event){
+   		if (pointInRect(event.clientX,event.clientY,rect)){
+      		context.fillStyle = "#f66";
+      }
+      else {
+      		context.fillStyle = "#999";
+      }
+      context.fillRect(rect.x,rect.y, rect.width, rect.height)
+      
+   })
+
+ }
